336f8e522b3f5c35feb0432c9ebf96bfd2b53ac8
kennethdevpc <kennethdevpc@gmail.com>
2023-04-05 15:07:03 -0500

    pasos

--- front/auth-admin/src/app/app-routing.module.ts
+++ front/auth-admin/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PublicComponent} from "./public/public.component";
+import {SecureComponent} from "./secure/secure.component";
+import {HomeComponent} from "./public/home/home.component";
+import {LoginComponent} from "./public/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    component: PublicComponent,
+    children:[
+      { path: 'home', component: HomeComponent, data: { title: 'home' }  },
+      { path: 'login', component: LoginComponent, data: { title: 'login' }  },
+    ]
+  },
+
+  { path: 'secure', component: SecureComponent, data: { title: 'public' }  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
